66cbd251e4fac6d21af7dba9be17b8fc95769c7a
Sofi Tymechko <sofiatymechko@gmail.com>
2022-11-25 21:38:35 +0200

    create Header

--- youi-ui/src/app/App.jsx
+++ youi-ui/src/app/App.jsx
@@ -2,11 +2,13 @@ import MuiTheme from "./MuiTheme";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 import "@fontsource/nunito-sans";
+import Header from "./Header";
 
 const App = () => {
   return (
     <MuiTheme>
       <BrowserRouter>
+        <Header />
         <Router />
       </BrowserRouter>
     </MuiTheme>
