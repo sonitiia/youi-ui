a29fa09f9d1d70a1ce647dbd86c4f96807e24cbd
Sofi Tymechko <sofiatymechko@gmail.com>
2022-11-27 18:31:55 +0200

    configure colors and screen sizes for MuiTheme

--- youi-ui/src/app/MuiTheme.jsx
+++ youi-ui/src/app/MuiTheme.jsx
@@ -1,18 +1,35 @@
-import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import * as React from "react";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 
 export const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+            xxl: 1972,
+            xxxl: 2508,
+            exl: 3208,
+            eexl: 4008,
+            eeexl: 5000,
+            eeexxl: 6100,
+            eeexxxl: 7300,
+            eeexxxxl: 8500,
+        },
+    },
     palette: {
         type: 'dark',
         primary: {
             main: '#000000',
-            light: '#FFFFFF',
+            light: 'rgba(265,265,265,0.2)',
             dark: '#000000',
         },
         secondary: {
-            main: '#73428a',
-            light: '#9f73b9',
-            dark: '#4c215a',
+            main: 'rgba(255,255,255,0.95)',
+            light: '#ffffff',
+            dark: 'rgba(255,245,235,0.87)',
         },
         error: {
             main: '#e23526',
@@ -28,7 +45,7 @@ export const theme = createTheme({
             paper: 'rgba(255,243,224,0.73)',
         },
         text: {
-            primary: 'rgba(255,255,255,0.87)',
+            primary: 'rgba(255,255,255,0.9)',
             secondary: '#fffded',
             disabled: 'rgba(218,218,218,0.29)',
             hint: '#ffcdd2',
@@ -48,7 +65,7 @@ export const theme = createTheme({
                 html: {
                     minHeight: "100%",
                     fontFamily: "Nunito Sans",
-                    background: "linear-gradient(66deg, #f3ecd1, #f5dddd)",
+                    background: "#ffffff",
                 },
                 body: {
                     height: "100%",
