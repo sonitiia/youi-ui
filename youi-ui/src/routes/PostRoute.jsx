4515d7ecd539dcebf95ced547b026a5f6a221ae5
Sofi Tymechko <sofiatymechko@gmail.com>
2022-11-27 18:39:24 +0200

    edit styles according to the new MuiTheme cofiguration in PostRoute

--- youi-ui/src/routes/PostRoute.jsx
+++ youi-ui/src/routes/PostRoute.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Box, Button, Card, IconButton } from "@mui/material";
-import Post from "../img/sunset-small.jpg";
 import UserAndAvatar from "../components/UserAndAvatar";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CommentList from '../components/comment/CommentList';
@@ -10,58 +9,63 @@ const PostRoute = () => {
         <Card
             elevation={0}
             sx={{
+                boxShadow: "0 0 55px #dcdee0",
+                background: (theme) => theme.palette.secondary.main,
                 maxWidth: "1300px",
                 width: "80%",
-                height: "700px",
+                height: { xs: "800px", md: "700px" },
                 my: 10,
                 mx: "auto",
                 display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                borderRadius: 2,
+                flexDirection: { xs: "column", md: "row" },
+                justifyContent: { xs: "flex-start", md: "space-between" },
+                borderRadius: 8,
             }}>
             <Card
                 elevation={0}
                 sx={{
-                    height: "100%",
-                    maxWidth: "650px",
-                    width: "50%",
-                    backgroundImage: `url(${Post})`,
+                    minHeight: { xs: "50%", md: "100%" },
+                    maxWidth: { xs: "100%", md: "650px" },
+                    width: { xs: "100%", md: "50%" },
+                    backgroundImage: `url(${"https://i.pinimg.com/564x/aa/82/02/aa82021347e8729119e4ca8737e0ba34.jpg"})`,
                     backgroundRepeat: "no-repeat",
                     backgroundSize: "cover",
                     backgroundPosition: "top",
-                    borderRadius: 2,
+                    borderRadius: 7,
                 }}>
             </Card>
             <Box
                 sx={{
-                    width: "50%",
+                    width: { xs: "100%", md: "50%" },
                     display: "flex",
                     flexDirection: "column",
-                    // alignItems: "center",
-                    p: 10,
+                    px: 6,
+                    py: 8
                 }}>
                 <Box sx={{
                     display: "flex",
                     flexDirection: "row",
                     alignItems: "center",
+                    justifyContent: "space-between",
                 }}
                 >
                     <UserAndAvatar />
-                    <IconButton sx={{ p: 0, width: 50, height: 50 }}>
-                        <MoreVertIcon sx={{ color: (theme) => theme.palette.primary.main }} />
-                    </IconButton>
-                    <Button
-                        sx={{
-                            height: 50,
-                            width: 90,
-                            borderRadius: 2,
-                            bgcolor: (theme) => theme.palette.primary.main,
-                            color: (theme) => theme.palette.text.primary,
-                            ml: 1
-                        }}>
-                        save
-                    </Button>
+                    <Box>
+                        <IconButton sx={{ p: 0, width: 30, height: 30 }}>
+                            <MoreVertIcon sx={{ color: (theme) => theme.palette.primary.main }} />
+                        </IconButton>
+                        <Button
+                            sx={{
+                                height: 50,
+                                width: 90,
+                                borderRadius: 2,
+                                bgcolor: (theme) => theme.palette.primary.main,
+                                color: (theme) => theme.palette.text.primary,
+                                ml: 1
+                            }}>
+                            save
+                        </Button>
+                    </Box>
                 </Box>
                 <CommentList />
             </Box>
