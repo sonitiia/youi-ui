5b93ded76a465d4df58c040d4a23044f837106b5
Sofi Tymechko <sofiatymechko@gmail.com>
2022-11-27 18:43:52 +0200

    change maxWidth in FullWidthContainer according to new MuiTheme configured sizes

--- youi-ui/src/app/layouts/FullWidthContainer.jsx
+++ youi-ui/src/app/layouts/FullWidthContainer.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const FullWidthContainer = () => {
     return (
-        <Container disableGutters maxWidth="xxxl">
+        <Container disableGutters maxWidth="eeexxxxl">
             <Outlet />
         </Container>
     );
